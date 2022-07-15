1b1cfb8af95a876aa75f701d01475d9dd1689e0c
siddhesh upkare <supkare@chirpn.com>
2022-07-15 22:41:47 +0530

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAhKFQlFtyGl7fahxnIAVfCZcs7wyfTAps",
+    authDomain: "garbh-sanskar-app.firebaseapp.com",
+    projectId: "garbh-sanskar-app",
+    storageBucket: "garbh-sanskar-app.appspot.com",
+    messagingSenderId: "534636719476",
+    appId: "1:534636719476:web:a5e3d22f6de4afe22f1021",
+    measurementId: "G-747G541XH5"
+  }
 };
