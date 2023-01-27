1bbaecd2a9fecd65f12e4879b268ad2febb8f0cd
siddhesh upkare <supkare@chirpn.com>
2023-01-27 18:36:49 +0530

    deploy test

--- server.js
+++ server.js
@@ -5,4 +5,5 @@ app.use(express.static(__dirname + '/dist/angularadmin'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/myapp/index.html'));});
+
 app.listen(process.env.PORT || 8080);
